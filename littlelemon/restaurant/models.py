840695585a7c1fee270cd72dd7d83970a280b742
Chia Kian An <kianan@gmail.com>
2024-09-27 11:36:05 +0800

    fix: change booking date to DateField

--- littlelemon/restaurant/models.py
+++ littlelemon/restaurant/models.py
@@ -12,7 +12,7 @@ class Menu(models.Model):
 class Booking(models.Model):
     name = models.CharField(max_length=255)
     no_of_guests = models.IntegerField()
-    booking_date = models.DateTimeField()
+    booking_date = models.DateField()
 
     def __str__(self):
         return self.name
